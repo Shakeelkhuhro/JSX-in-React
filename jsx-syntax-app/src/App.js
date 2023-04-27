d35d95a09de3dd8426b001d93a029a610d549230
Shakeel Khuhro <shakeelahmedkhuhro5454@gmail.com>
2023-04-27 19:07:50 +0500

    first commit

---
+++ jsx-syntax-app/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+let name="Shakeel Khuhro";
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contac</li>
+      <li>Help</li>
+      <div>
+        <h1>Hello {name}</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil ipsa doloribus iste laborum, architecto, laboriosam cumque quas sequi repellendus obcaecati nulla delectus omnis ipsum repellat quibusdam quos sed autem. Nesciunt atque fuga praesentium voluptates provident accusamus cum beatae rem sit? Repellendus consequatur sit illo sed id autem impedit, iste ratione.</p>
+      </div>
+    </nav>
+    </>
+  );
+}
+
+export default App;
